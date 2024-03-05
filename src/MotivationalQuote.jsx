26529b70858d65eea97fa1004b52332638c4e668
Tianna <140460149+tianna-spears@users.noreply.github.com>
2024-03-05 22:49:44 +0000

    moved list into new TodoList Component, created AddTodoForm Component, and bonus-- added new Component called MotivationalQuote to App.jsx

---
+++ src/MotivationalQuote.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MotivationalQuote () {
+    return (
+        <h3><em> "First, forget inspiration. Habit is more dependable. Habit will sustain you
+        whether you're inspired or not. Habit will help you finish and polish your stories. 
+        Inspiration won't. Habit is persistence in practice." <br></br> - Octavia Butler
+        </em></h3>
+    )
+}
+
+
+
+export default MotivationalQuote;
