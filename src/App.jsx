91ebce72d3d90ca5aca799edc5a6b206025f3461
Tianna <140460149+tianna-spears@users.noreply.github.com>
2024-04-23 15:23:50 +0100

    Lesson 1.7 Homework- Part 1 of 4, Remove Custom Hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,18 @@ import TodoList from './TodoList.jsx';
 import AddTodoForm from './AddTodoForm.jsx';
 import MotivationalQuote from './MotivationalQuote.jsx';
 
-function useSemiPersistentState (key, initialState) {
-    const [value, setValue] = useState(
-    JSON.parse(localStorage.getItem(key)) || initialState
+function App() {
+  const key = 'savedTodoList';
+  const initialState = [];
+
+  const [todoList, setTodoList] = useState(
+    () => JSON.parse(localStorage.getItem(key)) || initialState
   );
   
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
-  }, [value, key]);
+    localStorage.setItem(key, JSON.stringify(todoList));
+  }, [todoList, key]);
   
-  return [value, setValue];
-};
-
-function App() {
-  const [todoList, setTodoList] = useSemiPersistentState('savedTodoList', []);
-
     const removeTodo = (id) => {
       const updatedTodoList = todoList.filter((todo) => todo.id !== id);
     setTodoList(updatedTodoList);
